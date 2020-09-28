fde24116671c98e8c4e1e068dff91a8cb1e00554
lzy <lzy>
2020-09-28 23:58:36 +0800

    new project

---
+++ project/config-server/src/main/java/com/project/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigApplication.class);
+    }
+}
